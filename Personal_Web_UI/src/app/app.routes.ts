cb46e598fee083ef17fcc3bb6c1353d4086f23b3
mauritalkie <93022536+mauritalkie@users.noreply.github.com>
2024-03-23 15:31:58 -0600

    viewer: only html and some css alignments

--- Personal_Web_UI/src/app/app.routes.ts
+++ Personal_Web_UI/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/viewer/home/home.component';
+import { PortfolioComponent } from './components/viewer/portfolio/portfolio.component';
+import { AboutMeComponent } from './components/viewer/about-me/about-me.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+    {path: 'portfolio', component: PortfolioComponent},
+    {path: 'about_me', component: AboutMeComponent}
+];

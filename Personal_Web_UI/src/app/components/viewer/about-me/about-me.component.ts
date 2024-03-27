5e3638c94a26cb4311e43c8aa8e3e17c71f8a990
mauritalkie <93022536+mauritalkie@users.noreply.github.com>
2024-03-26 19:31:56 -0600

    viewer: navbar as component and adding styles to about me

--- Personal_Web_UI/src/app/components/viewer/about-me/about-me.component.ts
+++ Personal_Web_UI/src/app/components/viewer/about-me/about-me.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.css'
 })
--- Personal_Web_UI/src/app/components/viewer/portfolio/portfolio.component.ts
+++ Personal_Web_UI/src/app/components/viewer/portfolio/portfolio.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css'
 })

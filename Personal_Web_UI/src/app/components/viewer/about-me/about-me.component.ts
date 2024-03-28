843626df3fac5bd0836d544bacff8df44af5a2c0
mauritalkie <93022536+mauritalkie@users.noreply.github.com>
2024-03-27 18:56:05 -0600

    binding select to about me options

--- Personal_Web_UI/src/app/components/viewer/about-me/about-me.component.ts
+++ Personal_Web_UI/src/app/components/viewer/about-me/about-me.component.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FormsModule, CommonModule],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.css'
 })
 export class AboutMeComponent {
+  selectedOption: string = '';
+  
+  options = [
+    {value: "about_me", text: "About me"},
+    {value: "skills", text: "Skills"},
+    {value: "experience", text: "Experience"},
+    {value: "future_projects", text: "Future Projects"}
+  ];
 
+  onOptionChange() {
+    console.log(this.selectedOption);
+
+    //TODO: call the API and save description in a new variable, then bind it to <p>
+
+    /* if (this.selectedOption === 'about_me') {
+      this.selectedOption = 'about_me';
+    } else if (this.selectedOption ==='skills') {
+      this.selectedOption ='skills';
+    } else if (this.selectedOption === 'experience') {
+      this.selectedOption = 'experience';
+    } else if (this.selectedOption === 'future_projects') {
+      this.selectedOption = 'future_projects';
+    } */
+
+  }
+
+  ngOnInit() {
+    this.selectedOption = this.options[0].value;
+  }
 }

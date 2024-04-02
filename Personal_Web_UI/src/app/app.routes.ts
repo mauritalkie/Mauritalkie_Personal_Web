70fba5812e005a7582653625f8ffc7d02be0b373
mauritalkie <93022536+mauritalkie@users.noreply.github.com>
2024-04-01 20:53:06 -0600

    adding owner navbar and changing viewer navbar

--- Personal_Web_UI/src/app/app.routes.ts
+++ Personal_Web_UI/src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/viewer/home/home.component';
 import { PortfolioComponent } from './components/viewer/portfolio/portfolio.component';
 import { AboutMeComponent } from './components/viewer/about-me/about-me.component';
+import { AdminHomeComponent } from './components/owner/home/home.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
     {path: 'portfolio', component: PortfolioComponent},
-    {path: 'about_me', component: AboutMeComponent}
+    {path: 'about_me', component: AboutMeComponent},
+
+    {path: 'admin/home', component: AdminHomeComponent}
 ];

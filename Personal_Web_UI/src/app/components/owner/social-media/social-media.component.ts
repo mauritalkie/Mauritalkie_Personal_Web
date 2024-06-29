de76dbf98c78aead6d4d7d68bad52dc47f5a19d9
mauritalkie <93022536+mauritalkie@users.noreply.github.com>
2024-06-28 23:36:36 -0600

    adding modal component and styles

--- Personal_Web_UI/src/app/components/owner/social-media/social-media.component.ts
+++ Personal_Web_UI/src/app/components/owner/social-media/social-media.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { GetSocialMedia } from '../../../dtos/SocialMedia';
+import { ModalComponent } from '../modal/modal.component';
 
 @Component({
   selector: 'app-social-media',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, ModalComponent],
   templateUrl: './social-media.component.html',
   styleUrl: './social-media.component.css'
 })
 export class SocialMediaComponent {
+  isModalVisible: boolean = false;
+
   socialMedia: GetSocialMedia[] = [
     {Id: 1, SocialMediaName: 'quora', SocialMediaUrl: 'quora.com'},
     {Id: 2, SocialMediaName: 'facebook', SocialMediaUrl: 'facebook.com'},
@@ -23,5 +26,13 @@ export class SocialMediaComponent {
 
   deleteSocialMedia(id: any) {
     this.socialMedia = this.socialMedia.filter(item => item.Id!== id);
+  }
+
+  openModal() {
+    this.isModalVisible = true;
+  }
+
+  closeModal() {
+    this.isModalVisible = false;
   }
 }
